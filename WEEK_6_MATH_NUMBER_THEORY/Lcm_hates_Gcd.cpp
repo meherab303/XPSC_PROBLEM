dbfbcdb780c6791675714cec7767ba07736a36bb
meherab nahin <nahinrahman87@gmail.com>
2025-12-03 13:05:16 +0600

    lcm hates gcd

--- WEEK_6_MATH_NUMBER_THEORY/Lcm_hates_Gcd.cpp
+++ WEEK_6_MATH_NUMBER_THEORY/Lcm_hates_Gcd.cpp
@@ -8,13 +8,9 @@ int main()
   int t;cin>>t;
   while(t--){
     int a,b;cin>>a>>b;
-    int gcd=__gcd(a,b);
+    int gcd=__gcd(a,b);  //lcm baranu jabena.gcd baraite hbe.tahole a,b er gcd e hbe x.tahole a b both x dara divisible.lcm(a,x)=max_element.mane a e hbe.gcd(b,x)=min_element mane x e hbe.gcd er theke beshi baranu jabena.baraile lcm bere jabe.
     cout<<a-gcd<<endl;
   }  
-    
-
-  
-   
   return 0;
 }
 
