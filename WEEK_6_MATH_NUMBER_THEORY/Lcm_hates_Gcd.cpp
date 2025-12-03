5ddc92960f45f8d4172d93e7a9fda219b16a6437
meherab nahin <nahinrahman87@gmail.com>
2025-12-03 13:00:50 +0600

    subset gcd

---
+++ WEEK_6_MATH_NUMBER_THEORY/Lcm_hates_Gcd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;cin>>t;
+  while(t--){
+    int a,b;cin>>a>>b;
+    int gcd=__gcd(a,b);
+    cout<<a-gcd<<endl;
+  }  
+    
+
+  
+   
+  return 0;
+}
+
---
+++ WEEK_6_MATH_NUMBER_THEORY/Subset_GCD.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;cin>>t;
+    while(t--){
+        int n,k;
+	cin>>n>>k;
+	int max_gcd=n/k;
+
+	int i=max_gcd;
+	while(k--){
+	      cout<<i<<" ";
+	      i+=max_gcd;
+	}
+	
+	cout<<endl;
+    }
+	
+
+}
+
+// n element er k subset er max_gcd n/k e hoite pare.er theke beshi kokhonoi possible na.
+
+//যদি subset-এর প্রতিটি element max_gcd-এর গুণিতক হয় এবং সেই element-গুলো 1..N এর ভেতরে থাকে .তাহলে subset-এর GCD হবে max_gcd এবং এর চেয়ে বড় GCD সম্ভবই না, কারণ
+// GCD কখনোই (N/k)-এর বেশি হতে পারে না।
