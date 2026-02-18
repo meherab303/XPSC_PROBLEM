67e1ea851cdca092981137fa26463c0a4f9297b7
meherab nahin <nahinrahman87@gmail.com>
2026-02-18 13:02:08 +0600

    interesting drink

---
+++ Searching_&_Sorting/Problem/B_Interesting_drink.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;cin>>n;
+  vector<int>v(n);
+  for(int i=0;i<n;i++){
+    cin>>v[i];
+  }
+  sort(v.begin(),v.end());
+  int q;cin>>q;
+  for(int i=0;i<q;i++){
+    int key;cin>>key;
+    auto it=upper_bound(v.begin(),v.end(),key);
+    int total_shops=it-v.begin();
+    cout<<total_shops<<endl;
+  }  
+    
+
+  
+   
+  return 0;
+}
+
