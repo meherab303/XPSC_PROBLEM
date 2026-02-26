461195d837b075b6ec45fbda7048ad1a1311d2d9
meherab nahin <nahinrahman87@gmail.com>
2026-02-26 19:23:36 +0600

    lambda functor

---
+++ Super_Zone/searching_sorting_2/lambad_func.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  
+  //synta
+  /*
+  [capture](parameters)->return type{
+  //body
+  
+  }
+
+  //capture clause
+
+  1)copy by value:
+  int x=20;
+
+  auto val=[x](){
+    cout<<x<<endl;
+  };
+  val();
+
+  2)capture by referense
+
+  auto val=[&x](){
+    cout<<x<<endl;
+  };
+  val();
+
+  3)capture all by value
+
+  auto val=[=](){
+    cout<<x<<endl;
+  };
+  val();
+
+  4)capture all by reference
+
+  auto val=[&](){
+    cout<<x<<endl;
+  };
+  val();
+
+
+  //call instant without stroing in variable
+
+  [x](){
+    cout<<x<<endl;
+  }();
+  
+
+
+  //body te return type ektai hole c++ automatic type deduce kore nei
+
+  //lambda function holo ekta anonymous object.C++ এ lambda ashole  pure function na.c++ internally ekta  unnamed class banai.
+  
+  */
+
+
+
+  
+    
+
+  
+   
+  return 0;
+}
+
